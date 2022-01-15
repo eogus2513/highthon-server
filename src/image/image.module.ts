21ef17f0f1fa0a13e3208d61f397db652b8e1bf4
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-16 01:28:37 +0900

    ⚡️ :: AuthModule 추가

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entity/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   providers: [ImageService],
   exports: [ImageService],
 })
