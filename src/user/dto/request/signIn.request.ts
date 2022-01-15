24e95a13c0bbb97a135398df35d97f1067738aa4
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-15 15:42:47 +0900

    ⚡️ :: length 추가

--- src/user/dto/request/signIn.request.ts
+++ src/user/dto/request/signIn.request.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class SignInRequest {
+  @Length(5, 15)
   @IsString()
   @IsNotEmpty()
   id: string;
 
+  @Length(8, 30)
   @IsString()
   @IsNotEmpty()
   password: string;
