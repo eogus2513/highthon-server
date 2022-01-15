0d8e23bfc14cc9c06cb6ec7457d82a4847bc04d5
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-15 22:45:57 +0900

    ⚡️ :: Image Module 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeormConfigModule } from '../typeorm/typeorm-config.module';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
+import { ImageModule } from '../image/image.module';
 
 @Module({
-  imports: [TypeormConfigModule, AuthModule, UserModule],
+  imports: [TypeormConfigModule, AuthModule, UserModule, ImageModule],
   controllers: [AppController],
   providers: [AppService],
 })
